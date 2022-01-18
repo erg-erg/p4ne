ee20e9b841776090feeca554e3d4bb451abe06f3
erg-erg <88vsmirnov@gmail.com>
2022-01-18 15:28:06 +0300

    new commit

--- git_test_1.py
+++ git_test_1.py
@@ -1,4 +1,8 @@
-number = 1
-t2 = (1, 2, 3)
-print(type(t2))
-print(dir(number))
+#number = 1
+#t2 = (1, 2, 3)
+#print(type(t2))
+#print(dir(number))
+
+import sys
+s = 'some text'
+print(sys.getsizeof(s))
